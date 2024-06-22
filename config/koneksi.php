24d06ead1bd4ea9063c9a0ca62b41fc6420fcaf0
farizz12 <frigodafriyantoo@gmail.com>
2024-06-22 15:59:22 +0700

    Update koneksi.php

--- config/koneksi.php
+++ config/koneksi.php
@@ -1,22 +1,14 @@
 <?php
-// Retrieve environment variables
-$servername = getenv('DB_HOST') ?: 'localhost';
-$username = getenv('DB_USERNAME') ?: 'root';
-$password = getenv('DB_PASSWORD') ?: '';
-$dbname = getenv('DB_NAME') ?: 'WarungBakso';
+
+$db_name = 'mysql:host=db;dbname=warungbakso'; // Ganti dengan alamat IP container MySQL
+$user_name = 'warungbakso';
+$user_password = 'warungbakso';
 
 try {
-    // Create a new PDO instance
-    $dsn = "mysql:host=$servername;dbname=$dbname;charset=utf8mb4";
-    $options = [
-        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        PDO::ATTR_EMULATE_PREPARES   => false,
-    ];
-    $pdo = new PDO($dsn, $username, $password, $options);
-
-    echo "Koneksi berhasil";
-} catch (PDOException $e) {
-    die("Koneksi gagal: " . $e->getMessage());
+    $conn = new PDO($db_name, $user_name, $user_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
 }
+
 ?>
