0f7a2731c49048f535349dda9a1ab3fc053255fb
farizz12 <frigodafriyantoo@gmail.com>
2024-06-22 15:27:02 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+header('location: beranda');
+
+
+// Nama Anggota Kelompok:
+// 1. Dzikri Naufal Wisnu Pravida (2211104063)
+// 2. Muhammad Rizaldy Akbar (2211104065)
+// 3. Afif Ramadhani (2211104066)
+// 4. Muhammad Fariz Nur Hidayat (2211104069)
+// 5. Kholil Abdi Prasetyo (2211104071)
